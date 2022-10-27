ad9d44e7414e31656c6453cc5460c4d5b50bd086
sunnytechie <sunnyict001@gmail.com>
2022-10-27 08:41:43 +0100

    api fix

--- app/Http/Controllers/Api/AudioSeriesController.php
+++ app/Http/Controllers/Api/AudioSeriesController.php
@@ -2,8 +2,9 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Controllers\Controller;
+use App\Models\Audioserie;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class AudioSeriesController extends Controller
 {

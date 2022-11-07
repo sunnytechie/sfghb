482abd8345b40f255c867e0d81292bd9afcb57e7
sunnytechie <sunnyict001@gmail.com>
2022-11-08 00:41:48 +0100

    sfghb payment updated

--- app/Http/Controllers/Api/PaymentController.php
+++ app/Http/Controllers/Api/PaymentController.php
@@ -23,34 +23,44 @@ class PaymentController extends Controller
 
     public function store(Request $request) {
         //Update validity if email already exits else store details
-
         $validity = Carbon::today()->addDays(90);
-        //print_r($date);
+        $checkEmailExist = Payment::select('email')->where('email', $request->email)->first();
+       
+        if ($checkEmailExist) {
+            $payment = Payment::where('email', $request->email)->first();
+            $payment->validity = $validity;
+            $payment->currency = $request->currency;
+            $payment->amount = $request->amount;
+            $payment->country = $request->country;
+            $payment->method = $request->method;
+            $payment->tx_ref = $request->tx_ref;
+            $payment->save();
 
-        request()->validate([
-            'name' => 'required',
-            'email' => 'required',
-            'currency' => 'required',
-            'amount' => 'required',
-            'country' => '',
-            'method' => 'required',
-            'tx_ref' => 'required',
-        ]);
+        } else {
+            request()->validate([
+                'name' => 'required',
+                'email' => 'required',
+                'currency' => 'required',
+                'amount' => 'required',
+                'country' => '',
+                'method' => 'required',
+                'tx_ref' => 'required',
+            ]);
+    
+            $payment = new Payment;
+            $payment->name = $request->name;
+            $payment->email = $request->email;
+            $payment->currency = $request->currency;
+            $payment->amount = $request->amount;
+            $payment->country = $request->country;
+            $payment->validity = $validity;
+            $payment->method = $request->method;
+            $payment->tx_ref = $request->tx_ref;
+            $payment->save();
+        }
 
-        $payment = new Payment;
-        $payment->name = $request->name;
-        $payment->email = $request->email;
-        $payment->currency = $request->currency;
-        $payment->amount = $request->amount;
-        $payment->country = $request->country;
-        $payment->validity = $validity;
-        $payment->method = $request->method;
-        $payment->tx_ref = $request->tx_ref;
-        $payment->save();
-
-        $status = Payment::where('email', $payment->email)->first();
         return response()->json([
-            'status' => $status,
+            'status' => "You have successfully paid for searching for Gods heartbeat, thank you."
         ]);
     }
 }

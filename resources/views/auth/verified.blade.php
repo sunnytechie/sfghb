143020d658fee76b95c6a264d9b3df6fccaa395d
sunnytechie <sunnyict001@gmail.com>
2023-06-17 16:05:13 +0100

    new

--- resources/views/auth/verified.blade.php
+++ resources/views/auth/verified.blade.php
@@ -6,8 +6,8 @@
             <div class="card">
               <div class="card-body text-center">
                   <img class="my-3 text-center" src="{{ asset('assets/img/verified.svg') }}" alt="">
-                  <h4 style="font-style: normal; font-weight: 700; font-size: 20px; line-height: 160%; text-align: center; letter-spacing: -0.3px; color: #000000;">Verification Complete</h4>
-                  <p style="font-style: normal; font-weight: 400; font-size: 12px; line-height: 160%; letter-spacing: -0.3px; color: #000000;">Your Account has been set up and verified successfully</p>
+                  <h4 style="font-style: normal; font-weight: 700; font-size: 20px; line-height: 160%; text-align: center; letter-spacing: -0.3px; color: #000000;">Welcome to SFGHB.</h4>
+                  <p style="font-style: normal; font-weight: 400; font-size: 12px; line-height: 160%; letter-spacing: -0.3px; color: #000000;">You can now login on the mobile application.</p>
                     <a href="https://play.google.com/store/apps/details?id=godsheartbeat.net" class="btn btn-default btn-sm mt-1" style="font-style: normal; font-weight: 400; font-size: 14px; line-height: 160%; letter-spacing: -0.3px; color: #0080E6;">Proceed to Mobile App</a>
               </div>
             </div>
